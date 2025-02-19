cce10bcace2b6db781bcc987b2f19df3d3d3e580
MatheusGimenesDeAlmeida <mthga500@gmail.com>
2025-02-19 10:37:36 -0300

    .

--- main_isr_kiss.c
+++ main_isr_kiss.c
@@ -1,20 +1,20 @@
 #include "asf.h"
 
 volatile int g_cnt = 0;
-char g_str[10];
 volatile int apertado = 0;
 // This code creates a progress bar on an OLED screen that
 // increases when the button is pressed.
 void btn_callback(void) {
   apertado = 1; 
-  if (g_cnt >= 8)
-    g_cnt = 0;
   g_cnt = 2;
 }
 
 void main(void) {
   if (apertado){
-  printf("btn pressed \n");}
+  printf("btn pressed \n");
+  if (g_cnt >= 8);
+    g_cnt = 0;}
+  char g_str[10];
   for (i = 0; i < g_cnt; i++) {
     g_str = 1;
     g_str[i] = '*';
