04cfbf2a2e861ecf1c48971a92caae31da6a455d
MatheusGimenesDeAlmeida <mthga500@gmail.com>
2025-02-19 10:24:25 -0300

    .

--- main_isr_kiss.c
+++ main_isr_kiss.c
@@ -18,7 +18,7 @@ void main(void) {
   for (i = 0; i < g_cnt; i++) {
     g_str = 1;
     g_str[i] = '*';
-    g_str[i + 1] = NULLL;
+    g_str[i + 1] = NULL;
     delay_ms(50);
     gfx_mono_draw_string(g_str, 0, 0, &sysfont);
 }
